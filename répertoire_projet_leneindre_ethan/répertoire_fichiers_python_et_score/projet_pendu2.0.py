4f97ed96f24a1a29743325531f5c95ffd9682022
TintinLeGrand <61232418+TintinLeGrand@users.noreply.github.com>
2021-01-27 17:20:23 +0100

    Add files via upload

--- répertoire_projet_leneindre_ethan/répertoire_fichiers_python_et_score/projet_pendu2.0.py
+++ répertoire_projet_leneindre_ethan/répertoire_fichiers_python_et_score/projet_pendu2.0.py
@@ -16,6 +16,7 @@ def choix_du_mot(liste_dictee):
     a=randint(0,len(liste_dictee)-1)
     mot= liste_dictee[a]
     del liste_dictee[a]
+    print(mot)
     return mot, liste_dictee
 
 def transformation_mot(mot):
@@ -23,6 +24,7 @@ def transformation_mot(mot):
     mot_vide=[]
     for i in range(len(mot)):
         mot_vide.append('_')
+    print(mot_vide)
     return mot_vide
 
 def detection_victoire():
@@ -32,35 +34,45 @@ def detection_victoire():
     else:
         pass
 
-def faute(x):
-    faute=f+x
-    x+=1
-    return x, faute
-
 def detection_lettre(a,b):
     commun=0
+    etat=False
     for i in range(len(mot)):
         lettre= mot[i]
         if lettre==a:
             commun+=1
-        if commun==0:
-            return faute, mot_vide
-        return mot_vide
+    if commun==0:
+        b+=1
+        etat=True
+    return b, mot_vide, etat
+    
+def enregistrer_score(score):
+    with open('score.txt','a') as f:
+        f.write(str(score)+'\n \n')
+    
+def reset():
+    f=0
+    x=0
+    faute=0
+    return f, x, faute
 
-'''Code principal et m*rdier'''
+'''Code fumier'''
+phrase_bien=["J'ai un champion en face de moi !", "Tu es trop fort !", "Bonne réponse !", "Ouah, mais tu triches ?", "T'es le plus fort"]
 score=0
-f=0
-x=0
 liste_dictee, nombre_de_mots= enregistrement_des_mots()
-while len(liste_dictee)!=0:
-    x, faute=faute(x)
+nombre_mots=len(liste_dictee)
+while nombre_mots>1:
+    f, x, faute=reset()
     mot, liste_dictee=choix_du_mot(liste_dictee)
     mot_vide=transformation_mot(mot)
-    while faute!=7 or detection_victoire()!=True:
+    while f!=1:
         a=input('Choisissez une lettre')
-        mot_vide=detection_lettre(a,faute)
-        print(mot_vide)
-        pass
-        
+        faute, mot_vide, etat=detection_lettre(a,faute)
+        if faute>=7:
+            f=1
+        affichage_du_mort(faute)
+        if etat==False:
+            print(phrase_bien[randint(0,4)])
     if detection_victoire==True:
-        pass
+        print(phrase_victoire(randint(1,6)))
+        
